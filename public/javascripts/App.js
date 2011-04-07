5ca0c55121d2257cc627bd210cdd178e23508cbb
Matt Haynes <matt@matthaynes.net>
2011-04-08 00:01:21 +0100

    Format

--- public/javascripts/App.js
+++ public/javascripts/App.js
@@ -1,11 +1,13 @@
 $(function() {
-    var drawer = new Drawer(document.getElementById('paper'));	
-	function drawIt() {                                         
-		drawer.draw(Parser.parse(document.getElementById('actual_code').value));		
-	}
-	$('#code').submit(function() {
-		drawIt()
-		return false;
-	});      
-	drawIt();
+	$('#code').each(function() {
+	  var drawer = new Drawer(document.getElementById('paper'));	
+		function drawIt() {                                         
+			drawer.draw(Parser.parse(document.getElementById('actual_code').value));		
+		}
+		$(this).submit(function() {
+			drawIt()
+			return false;
+		});      
+		drawIt();
+	});
 });
