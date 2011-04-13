a31d9eb8657b96cabc15779932e05ddd0611939c
Matt Haynes <matt@matthaynes.net>
2011-04-13 21:34:00 +0100

    Add more math

--- public/javascripts/language/commands/Math.js
+++ public/javascripts/language/commands/Math.js
@@ -1,39 +1,169 @@
 (function(i) {
 	
+	// Util
+	function deg2rad(d) {
+		return d * Math.PI / 180;
+	}
+	
 	/**
 	 * @logo
-	 * @command sum <num> <num>
+	 * @command abs <num>
 	 * @description
-	 * Returns the sum of the two numbers
+	 * Returns the absolute value of a number
 	 * @example    
-	 * show sum 3 3
-	 * 6
+	 * show abs -33 ;=> 33
 	 */
-	i.command('sum', function(a, b) { 
-		return new Tokens.NUMBER(a.value + b.value );
-	});
-
+	i.command('abs', function(a) { 
+		return new Tokens.NUMBER(Math.abs(a.value));
+	});	          
+	
+	/**
+	 * @logo
+	 * @command arctan <num>
+	 * @description
+	 * Stands for arc tangent. Reports the arc tangent (the inverse function of the tangent) of its input.
+	 * @example    
+	 * show arctan 1 ;=> 45
+	 * @see tan cos sin 
+	 */
+	i.command('arctan', function(a) { 
+		return new Tokens.NUMBER(Math.atan(deg2rad(a.value)));
+	});      
+	
+	/**
+	 * @logo
+	 * @command cos <num>
+	 * @description
+	 * Stands for cosine. Reports the cosine of its input.
+	 * @example    
+	 * show cos 60 ;=> 0.5
+	 * @see tan sin 
+	 */
+	i.command('cos', function(a) { 
+		return new Tokens.NUMBER(Math.atan(deg2rad(a.value)));
+	});	 
+	
 	/**
 	 * @logo
 	 * @command difference <num> <num>
 	 * @description
 	 * Returns the difference of the two numbers
 	 * @example  
-	 * show difference 5 3
-	 * 2   	
+	 * show difference 5 3 ;=> 2
 	 */	
 	i.command('difference', function(a, b) { 
 		return new Tokens.NUMBER(a.value - b.value );
-	});
-	    
+	});    
+	
+	/**
+	 * @logo
+	 * @command exp <num>
+	 * @description
+	 * Stands for exponential. Reports the number to the power of the constant e.
+	 * @example  
+	 * show exp 1 ;=> 2.71828182846 
+	 * @see log ln
+	 */	
+	i.command('exp', function(a) { 
+		return new Tokens.NUMBER(Math.exp(a.value));
+	});	   
+	
+	/**
+	 * @logo
+	 * @command int <num>
+	 * @description
+	 * Stands for integer. Reports the integer portion of its input.
+	 * @example  
+	 * show int 9.9999 ;=> 9
+	 * show int 2.5 ;=> 2	
+	 * @see round
+	 */	
+	i.command('int', function(a) { 
+		return new Tokens.NUMBER(parseInt(a.value));
+	});                                
+	        
+	/**
+	 * @logo
+	 * @command ln <num>
+	 * @description
+	 * Stands for natural logarithm. Reports the natural logarithm (the logarithm in base e) of the number. Inverse of exp.
+	 * @example  
+	 * show ln 1 ;=> 0
+	 * @see log exp
+	 */	
+	i.command('ln', function(a) {               
+		return new Tokens.NUMBER(Math.log(a.value));
+	});                                
+
+	/**
+	 * @logo
+	 * @command log <num>
+	 * @description
+	 * Stands for logarithm. Reports the logarithm of the number
+	 * @example  
+	 * show log 100 ;=> 2
+	 * @see ln exp
+	 */	
+	i.command('log', function(a) { 
+		return new Tokens.NUMBER(Math.log(a.value) / Math.log(10));
+	});                                
+
+	/**
+	 * @logo
+	 * @command minus <num>
+	 * @description
+	 * Reports the additive inverse of its input. Minus must be used to report the additive inverse of a variable (minus :num instead of -:num)
+	 * @example  
+	 * show minus 5 ;=> -5
+	 * @see difference -
+	 */	
+	i.command('minus', function(a) { 
+		return new Tokens.NUMBER(a.value * -1);
+	});	
+
+	/**
+	 * @logo
+	 * @command pi
+	 * @description
+	 * Reports the constant PI 
+	 * @example  
+	 * show pi 3.14159265359 ;=> -5
+	 */	
+	i.command('pi', function() { 
+		return new Tokens.NUMBER(Math.PI);
+	});          
+	
+	/**
+	 * @logo
+	 * @command power <num> <num>
+	 * @description
+	 * Reports first number raised to the power of second number
+	 * @example  
+	 * show power 2 10 ;=> 1024
+	 */	
+	i.command('power', function(a, b) { 
+		return new Tokens.NUMBER(Math.pow(a.value,b.value));
+	});	
+	
+	/**
+	 * @logo
+	 * @command product <num> <num>
+	 * @description	
+	 * Returns the product of the two numbers
+	 * @example 
+	 * show product 3 3 ;=> 9
+	 */
+	i.command('product', function(a, b) { 
+		return new Tokens.NUMBER(a.value * b.value );
+	});	                      
+	
 	/**
 	 * @logo
 	 * @command quotient <num> <num>
 	 * @description	
 	 * Returns the quotient of the two numbers
 	 * @example     
-	 * show quotient 10 2
-	 * 5	
+	 * show quotient 10 2 ;=> 5
 	 */
 	i.command('quotient', function(a, b) { 
 		return new Tokens.NUMBER(a.value / b.value );
@@ -41,16 +171,14 @@
 	                      
 	/**
 	 * @logo
-	 * @command product <num> <num>
-	 * @description	
-	 * Returns the product of the two numbers
-	 * @example 
-	 * show product 3 3 
-	 * 9    	
+	 * @command sum <num> <num>
+	 * @description
+	 * Returns the sum of the two numbers
+	 * @example    
+	 * show sum 3 3 ;=> 6
 	 */
-	i.command('product', function(a, b) { 
-		return new Tokens.NUMBER(a.value * b.value );
+	i.command('sum', function(a, b) { 
+		return new Tokens.NUMBER(a.value + b.value );
 	});
-	
-		
+			
 })(Interpreter);
--- spec/javascripts/language/commands/MathSpec.js
+++ spec/javascripts/language/commands/MathSpec.js
@@ -1,47 +1,78 @@
 describe("Math Commands", function() {  
-	  	
-	describe("sum", function() {  
-		it('should return the sum of two numbers as the correct type', function() {
-			expect(interpret("sum 2 3").type).toEqual("NUMBER")
-			expect(interpret("sum 2 3").value).toEqual(5)			  
-			expect(interpret("sum 2.4 3.6").type).toEqual("NUMBER")
-			expect(interpret("sum 2.4 3.6").value).toEqual(6.0)			
-			expect(interpret("sum 2.4 3").type).toEqual("NUMBER")
-			expect(interpret("sum 2.4 3").value).toEqual(5.4)			
-		});
-	});
-	
-	describe("difference", function() {  
-		it('should return the difference of two numbers as the correct type', function() {
-			expect(interpret("difference 3 2").type).toEqual("NUMBER")
-			expect(interpret("difference 3 2").value).toEqual(1)			  
-			expect(interpret("difference 2.4 3.6").type).toEqual("NUMBER")
-			expect(interpret("difference 2.4 3.6").value.toFixed(1)).toEqual('-1.2')			
-			expect(interpret("difference 2.4 3").type).toEqual("NUMBER")
-			expect(interpret("difference 2.4 3").value.toFixed(1)).toEqual('-0.6')			
-		});
-	});	
 
-	describe("quotient", function() {  
-		it('should return the quotient of two numbers as the correct type', function() {
-			expect(interpret("quotient 9 3").type).toEqual("NUMBER")
-			expect(interpret("quotient 9 3").value).toEqual(3)			  
-			expect(interpret("quotient 9.0 3.0").type).toEqual("NUMBER")
-			expect(interpret("quotient 9.0 3.0").value).toEqual(3)			
-			expect(interpret("quotient 2.4 3").type).toEqual("NUMBER")
-			expect(interpret("quotient 2.4 3").value.toFixed(1)).toEqual('0.8')			
-		});
-	});
-
-	describe("product", function() {  
-		it('should return the product of two numbers as the correct type', function() {
-			expect(interpret("product 9 3").type).toEqual("NUMBER")
-			expect(interpret("product 9 3").value).toEqual(27)			  
-			expect(interpret("product 9.0 3.0").type).toEqual("NUMBER")
-			expect(interpret("product 9.0 3.0").value).toEqual(27)			
-			expect(interpret("product 2.4 3").type).toEqual("NUMBER")
-			expect(interpret("product 2.4 3").value.toFixed(1)).toEqual('7.2')			
-	  });
-	});
+	// because im lazy
+	function quickly(cmd, behaviour, expectations) {
+		describe(cmd, function() {
+			it(behaviour, function() {
+				expectations.forEach(function(e) {
+					 var logo = e[0], expected = e[1];
+					 expect(interpret(logo).type).toEqual("NUMBER")
+					 var eq = Math.abs(interpret(logo).value - expected) < 0.00001; // compare floats to this accuracy
+					 expect(eq).toEqual(true)					
+				})
+			})
+		})
+	}
 	
+	function deg2rad(d) {
+		return d * Math.PI / 180;
+	}
+	
+  // Commands
+	quickly("abs", "should return the absolute value of a numbers", [
+	 ["abs difference 3 10", 7], ["abs difference 3.87 10", 6.13]
+	]);
+	
+	quickly("arctan", "should return the arctan of the value in degrees", [
+	 ["arctan 90", Math.atan(deg2rad(90))], ["arctan 45", Math.atan(deg2rad(45))], ["arctan 1", Math.atan(deg2rad(1))]
+	]);	                                                                   
+
+	quickly("cos", "should return the cos of the value in degrees", [
+	 ["cos 90", Math.atan(deg2rad(90))], ["cos 45", Math.atan(deg2rad(45))]
+	]);	
+
+	quickly("difference", "should return the difference of two numbers", [
+	 ["quotient 9 3", 3], ["quotient 10 3", 10 / 3], ["quotient 2.4 3", 0.8]
+	]);                                                              
+	
+	quickly("exp", "should return the exponetial of the number", [
+	 ["exp 90", Math.exp(90)], ["exp 45.67", Math.exp(45.67)]
+	]);	  
+	
+	quickly("int", "should return the integer portion of the number", [
+	 ["int 90", 90], ["int difference 7.6 9", -1], ["int 2.43", 2]
+	]);	
+	         
+	quickly("ln", "should return the naturtal logarithm of the number", [
+	 ["ln 1", 0], ["ln 10", Math.log(10)], ["ln 67", Math.log(67)]
+	]);	
+	
+	quickly("log", "should return the logarithm of the number", [
+	 ["log 100", 2], ["log 10", Math.log(10) / Math.log(10)], ["log 67", Math.log(67)  / Math.log(10)]
+	]);	    
+	
+	quickly("minus", "should return the additive inverse of the number", [
+	 ["minus 100", -100], ["minus 1.234", -1.234], ["minus minus 5", 5]
+	]);	
+	
+	quickly("pi", "should return PI", [
+	 ["pi", Math.PI]
+	]);	                               
+	
+	quickly("power", "should return number raised to seond number", [
+	 ["power 10 2", 100], ["power 2 12", Math.pow(2, 12)], ["power 3.1 minus 4", Math.pow(3.1,-4)]
+	]);	                           
+          
+	quickly("product", "should return the product of two numbers", [
+	 ["product 9 3", 27], ["product 10 7", 70], ["product 2.4 3", 7.2]
+	]);
+	
+	quickly("quotient", "should return the quotient of two numbers", [
+	 ["difference 3 2", 1], ["difference 2.4 3.6", -1.2], ["difference 2.4 3", -0.6]
+	]);	
+		  	                                  
+	quickly("sum", "should return the sum of two numbers", [
+	 ["sum 2 3", 5], ["sum 2.4 3.6", 6], ["sum 2.4 3", 5.4]
+	]);
+
 });
